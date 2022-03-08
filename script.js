c5ed272f9d8b352fe77cadcdd12b220270566cfd
Camila Bernal <camilab1998@hotmail.com>
2022-03-07 19:58:41 -0500

    Seccion contact con estilos y funciones

--- script.js
+++ script.js
@@ -17,3 +17,37 @@
         $menu.classList.remove("is-active");
     })
 })(document);
+
+
+/* ******** Contact Form ******** */
+((d)=> {
+    const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", e => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/camilab1998@hotmail.com", {
+            method: "POST",
+            body: new FormData(e.target)
+        })
+        .then(res => (res.ok ? res.json() : Promise.reject(res)))
+        .then(json => {
+            console.log(json);
+            location.hash = "#thanks";
+            $form.reset();
+        })
+        .catch(err => {
+            console.log(err);
+            let message = err.statusText || "There's been an error, try again"
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+            $loader.classList.add("none");
+            setTimeout(()=> {
+                location.hash = "#close"
+            }, 3000);
+        });
+    });
+})(document)
